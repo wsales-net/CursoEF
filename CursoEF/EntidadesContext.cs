1c5fd1efa16541baa1b4d4a8780fd292395c976a
Wellington <well_connect@live.com>
2018-03-05 23:52:58 -0300

    1 - Cadastro de Usuario E Criação de Tabela

---
+++ CursoEF/EntidadesContext.cs
@@ -0,0 +1,15 @@
+﻿using CursoEF.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoEF
+{
+    public class EntidadesContext : DbContext
+    {
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
--- CursoEF/Program.cs
+++ CursoEF/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CursoEF.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,19 @@ namespace CursoEF
     {
         static void Main(string[] args)
         {
+            EntidadesContext contexto = new EntidadesContext();
+            contexto.Database.CreateIfNotExists();
+
+            Usuario usuario = new Usuario()
+            {
+                Nome = "Well",
+                Senha = "123"
+            };
+            contexto.Usuario.Add(usuario);
+
+            contexto.SaveChanges();
+
+            contexto.Dispose();
         }
     }
 }
